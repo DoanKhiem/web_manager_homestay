7835389f24f357f30c112d96bf4894fbda8e0b75
doankhiem <khiembi161219@gmail.com>
2024-03-27 22:16:53 +0700

    new project laravel

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('admin.index');
+    return view('welcome');
 });
-Route::get('/nhanvien', function () {
-    return view('admin.list.nhanvien');
-});
-Route::get('/add-nhanvien', function () {
-    return view('admin.add.nhanvien');
-});
-Route::get('/hopdonglaodong', function () {
-    return view('admin.list.hopdonglaodong');
-});
-// Route::get()
