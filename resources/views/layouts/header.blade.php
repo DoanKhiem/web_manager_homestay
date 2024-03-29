7b3d50734361686a5329523734f9621b7c5b09b8
khiemhome <khiembi161218@gmail.com>
2024-03-29 14:10:09 +0700

    logout and sidebar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,15 +9,14 @@
                 <b class="logo-icon ps-2">
                     <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                     <!-- Dark Logo icon -->
-                    <img src="{{asset('/assets/images/logo-icon.png')}}" alt="homepage" class="light-logo"
-                         width="25" />
+                    <img src="{{asset('/assets/images/logo-icon.png')}}" alt="homepage" class="light-logo" width="25" />
                 </b>
                 <!--End Logo icon -->
                 <!-- Logo text -->
                 <span class="logo-text ms-2">
-                            <!-- dark Logo text -->
-                            <img src="{{asset('/assets/images/logo-text.png')}}" alt="homepage" class="light-logo" />
-                        </span>
+                    <!-- dark Logo text -->
+                    <img src="{{asset('/assets/images/logo-text.png')}}" alt="homepage" class="light-logo" />
+                </span>
                 <!-- Logo icon -->
                 <!-- <b class="logo-icon"> -->
                 <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
@@ -34,7 +33,7 @@
             <!-- Toggle which is visible on mobile only -->
             <!-- ============================================================== -->
             <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
-                        class="ti-menu ti-close"></i></a>
+                    class="ti-menu ti-close"></i></a>
         </div>
         <!-- ============================================================== -->
         <!-- End Logo -->
@@ -46,14 +45,14 @@
             <ul class="navbar-nav float-start me-auto">
                 <li class="nav-item d-none d-lg-block">
                     <a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)"
-                       data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a>
+                        data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a>
                 </li>
                 <!-- ============================================================== -->
                 <!-- create new -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         <span class="d-none d-md-block">Create New <i class="fa fa-angle-down"></i></span>
                         <span class="d-block d-md-none"><i class="fa fa-plus"></i></span>
                     </a>
@@ -73,11 +72,11 @@
                 <!-- ============================================================== -->
                 <li class="nav-item search-box">
                     <a class="nav-link waves-effect waves-dark" href="javascript:void(0)"><i
-                                class="mdi mdi-magnify fs-4"></i></a>
-                    <form class="app-search position-absolute">
-                        <input type="text" class="form-control" placeholder="Search &amp; enter" />
-                        <a class="srh-btn"><i class="mdi mdi-window-close"></i></a>
-                    </form>
+                            class="mdi mdi-magnify fs-4"></i></a>
+                    {{--                    <form class="app-search position-absolute">--}}
+                    {{--                        <input type="text" class="form-control" placeholder="Search &amp; enter" />--}}
+                    {{--                        <a class="srh-btn"><i class="mdi mdi-window-close"></i></a>--}}
+                    {{--                    </form>--}}
                 </li>
             </ul>
             <!-- ============================================================== -->
@@ -89,7 +88,7 @@
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="mdi mdi-bell font-24"></i>
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -111,7 +110,7 @@
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle waves-effect waves-dark" href="#" id="2" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="font-24 mdi mdi-comment-processing"></i>
                     </a>
                     <ul class="
@@ -126,7 +125,7 @@
                                     <!-- Message -->
                                     <a href="javascript:void(0)" class="link border-top">
                                         <div class="d-flex no-block align-items-center p-10">
-                                                    <span class="
+                                            <span class="
                                 btn btn-success btn-circle
                                 d-flex
                                 align-items-center
@@ -141,7 +140,7 @@
                                     <!-- Message -->
                                     <a href="javascript:void(0)" class="link border-top">
                                         <div class="d-flex no-block align-items-center p-10">
-                                                    <span class="
+                                            <span class="
                                 btn btn-info btn-circle
                                 d-flex
                                 align-items-center
@@ -156,7 +155,7 @@
                                     <!-- Message -->
                                     <a href="javascript:void(0)" class="link border-top">
                                         <div class="d-flex no-block align-items-center p-10">
-                                                    <span class="
+                                            <span class="
                                 btn btn-primary btn-circle
                                 d-flex
                                 align-items-center
@@ -171,7 +170,7 @@
                                     <!-- Message -->
                                     <a href="javascript:void(0)" class="link border-top">
                                         <div class="d-flex no-block align-items-center p-10">
-                                                    <span class="
+                                            <span class="
                                 btn btn-danger btn-circle
                                 d-flex
                                 align-items-center
@@ -204,27 +203,33 @@
                     pro-pic
                   " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="{{asset('/assets/images/users/1.jpg')}}" alt="user" class="rounded-circle"
-                             width="31" />
+                            width="31" />
                     </a>
-                    <ul class="dropdown-menu dropdown-menu-end user-dd animated"
-                        aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="javascript:void(0)"><i
-                                    class="mdi mdi-account me-1 ms-1"></i> My Profile</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i
-                                    class="mdi mdi-wallet me-1 ms-1"></i> My Balance</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i
-                                    class="mdi mdi-email me-1 ms-1"></i> Inbox</a>
+                    <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-account me-1 ms-1"></i> My
+                            Profile</a>
+                        <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-wallet me-1 ms-1"></i> My
+                            Balance</a>
+                        <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-email me-1 ms-1"></i>
+                            Inbox</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i
-                                    class="mdi mdi-settings me-1 ms-1"></i> Account
+                        <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-settings me-1 ms-1"></i>
+                            Account
                             Setting</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i
-                                    class="fa fa-power-off me-1 ms-1"></i> Logout</a>
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <button type="submit" class="dropdown-item">
+                                <i class="fa fa-power-off me-1 ms-1"></i> Logout
+                            </button>
+                        </form>
+
                         <div class="dropdown-divider"></div>
                         <div class="ps-4 p-10">
-                            <a href="javascript:void(0)"
-                               class="btn btn-sm btn-success btn-rounded text-white">View Profile</a>
+                            <a href="javascript:void(0)" class="btn btn-sm btn-success btn-rounded text-white">View
+                                Profile</a>
                         </div>
                     </ul>
                 </li>
