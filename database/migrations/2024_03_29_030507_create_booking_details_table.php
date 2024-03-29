d5cb89f35e5f5fcdacf8300d854ac04692c965d1
khiemhome <khiembi161218@gmail.com>
2024-03-29 10:57:53 +0700

    create model and migration

---
+++ database/migrations/2024_03_29_030507_create_booking_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('room_id')->constrained();
+            $table->foreignId('menu_id')->constrained();
+            $table->string('customer_name');
+            $table->string('phone_number');
+            $table->string('booking_category');
+            $table->dateTime('checkin');
+            $table->dateTime('checkout');
+            $table->integer('total_time');
+            $table->float('booking_price');
+            $table->integer('menu_quantity');
+            $table->float('menu_price');
+            $table->integer('adult');
+            $table->integer('kid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_details');
+    }
+};
