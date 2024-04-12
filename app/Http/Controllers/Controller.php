60345cfe4b2b5388ed7172a10065bebf5ca11f03
khiemhome <khiembi161219@gmail.com>
2024-04-12 13:26:15 +0700

    data dashboard

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function dashboard()
+    {
+        $totalAmount = \App\Models\Booking::where('booking_status', 3)->sum('total_amount');
+        $rooms = \App\Models\Room::orderBy('created_at', 'desc')->get();
+        $menus = \App\Models\Menu::orderBy('created_at', 'desc')->get();
+        $utilities = \App\Models\Utility::orderBy('created_at', 'desc')->get();
+        return view('dashboard', compact('rooms', 'menus', 'utilities', 'totalAmount'));
+    }
 }
